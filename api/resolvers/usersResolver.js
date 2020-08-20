4b8ea2f795c998b6ed77b190decbd0e674fd746c
JulianaAmoasei <late.parrot@gmail.com>
2020-08-20 13:55:24 -0300

    add custom scalar datetime

--- api/resolvers/usersResolver.js
+++ api/resolvers/usersResolver.js
@@ -1,4 +1,14 @@
+const { GraphQLScalarType } = require('graphql');
+
 const resolvers = {
+  // That’s the downside to using custom scalars. They’re not supported out of the box, we need to make our custom logic available to any projects that want to use them. 
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'string de data e horário no formato ISO-8601',
+    serialize: (value) => value.toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value)
+  }),
   Query: {
     users: (_, __, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (_, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
